74bdad8bab85ab274eed064c9b462bce1c22928f
h1bernard <130392186+h1bernard@users.noreply.github.com>
2023-07-14 12:39:37 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
+    <h1>Test 14 Jul</h1>
     <div>
       Hello World. <Link href="/about">About</Link>
     </div>
